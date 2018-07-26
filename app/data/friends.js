c7a5b6b9f42c5f80041f69d2e7b0b852a3c23e56
Alec Inthalangsy <alecinthalangsy@Alecs-MacBook-Air.local>
2018-07-26 18:54:24 -0400

    initial site files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsList = [
+    {
+        name: "Johnny Bravo",
+        photo: "https://vignette.wikia.nocookie.net/johnnybravo/images/b/bb/Johnnyb001.gif/revision/latest/scale-to-width-down/250?cb=20120620184904",
+        scores: [
+            4,
+            3,
+            2,
+            1,
+            3,
+            1,
+            2,
+            4,
+            3,
+            4 
+        ]
+    }
+];
+
+module.exports = friendsList;
